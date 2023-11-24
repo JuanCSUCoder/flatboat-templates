5d1dfb7f944eb20688dbb74e3f2e846a3452a400
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2023-11-23 20:38:55 -0500

    Fix: Namespace

--- release.sh
+++ release.sh
@@ -4,4 +4,4 @@
 source env.sh
 
 # Publish Templates
-devcontainer templates publish -r ghcr.io -n juancsucoder/flatboat-templates/roboten_ws_iron_nogpu ./container-based/dev/iron/iron_nogpu
+devcontainer templates publish -r ghcr.io -n juancsucoder/flatboat-templates ./container-based/dev/iron/iron_nogpu
