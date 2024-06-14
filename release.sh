2cf98261dc4e2d08a2a9fd35e9113a9eff418c76
Juan Camilo Sánchez Urrego <jcsu2019369@gmail.com>
2024-06-14 14:42:33 -0500

    Feat: Publish noetic template

--- release.sh
+++ release.sh
@@ -9,3 +9,4 @@ source env.sh
 # Publish Templates
 devcontainer templates publish -r ghcr.io -n juancsucoder/flatboat-templates ./ws/iron/iron_nogpu
 devcontainer templates publish -r ghcr.io -n juancsucoder/flatboat-templates ./ws/humble/humble_nogpu
+devcontainer templates publish -r ghcr.io -n juancsucoder/flatboat-templates ./ws/noetic/noetic_nogpu
